c1780a84b0a3a63e88096407f9cdf7d220f35e06
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-28 16:59:12 +0500

    Update main.py

--- gui/main.py
+++ gui/main.py
@@ -1,7 +1,7 @@
 """
   Still under development not ready for external use
   Visually: 90% Done
-  Backend: 10%
+  Backend: 20%
 """
 
 
@@ -59,6 +59,7 @@ class FirstWindow(Screen):
 
 class SecondWindow(Screen):
     dialog = False
+    dialog2 = False
 
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -70,46 +71,79 @@ class SecondWindow(Screen):
         self.TOTAL_MONEY = [0, 0, 0, 0]
         self.TOTAL_PASSENGERS_ = [0, 0, 0, 0]
         self.TEXT_TO_DISPLAY = ''
+        self.user_time = ''
+        self.user_ticket = 0
 
-    def show_dialog(self, Jrny: int, tickets: int):
+    def show_dialog(self, jrny: int, tickets: int, freetick: int):
 
         """Create group creation popup"""
 
         close = MDFlatButton(text="No", on_release=self.close_dialog)
-        proceed = MDFlatButton(text="Yes")
+        proceed = MDFlatButton(text="Yes", on_release=self.yes_button_dialog)
         if not self.dialog:
             self.dialog = MDDialog(
-                text=f"You are about to buy Tickets for journey# {Jrny}",
-                title=f"Are you sure you want to buy {tickets} Tickets?",
-                #type="custom",
+                text=f"You are about to buy Tickets for journey# {jrny}",
+                title=f"Are you sure you want to buy {tickets} Tickets? You will recieve {freetick} Free Tickets",
+                # type="custom",
                 size_hint=(0.5, 1),
                 auto_dismiss=False,  # needed to allow Buttons to operate
                 buttons=[proceed, close]
             )
         self.dialog.open()
 
-    def close_dialog(self, obj):
-        self.dialog.dismiss()
-        self.ids.time_check.text = ''
-        self.ids.ticket_check.text = ''
-        self.dialog = True
+    def about_dialog(self):
+        close = MDFlatButton(text="Close", on_release=self.close_dialog)
+        if not self.dialog2:
+            self.dialog_ = MDDialog(
+                text=f"Just took inspirations form my previous script of the same problem but it was done on a cli"
+                     f"This took real hardwork and time as i had 0 knowledge about Kivy in general, saw alot of times "
+                     f"when i thought to end this project",
+                title=f"Created by: Shahzaib Ali aka Ali™",
+                # type="custom",
+                size_hint=(0.5, 1),
+                auto_dismiss=False,
+                buttons=[close]
+            )
+        self.dialog_.open()
+
+    def close_dialog(self, xobj):
+        try:
+            self.dialog.dismiss()
+            self.ids.time_check.text = ''
+            self.ids.ticket_check.text = ''
+            self.dialog = True
+        except AttributeError:
+            self.dialog_.dismiss()
+            self.dialog2 = True
 
     def logout(self):
         self.manager.current = "first"
         self.manager.transition.direction = 'right'
 
-    def check_time_and_ticket(self, widget):
-        user_time = str(self.ids.time_check.text)
-        user_ticket = str(self.ids.ticket_check.text)
-        if user_time in self.UP_TIME and user_ticket.isnumeric():
-            self.dialog = False
-            INDEX = self.UP_TIME.index(user_time)
-            INDEX = INDEX+1
-            self.show_dialog(tickets=int(user_ticket), Jrny=int(INDEX))
+    @staticmethod
+    def free_ticket(num):
+        if 10 <= num <= 80:
+            ticket = round(num / 10)
+            return int(ticket)
+        else:
+            return int(0)
 
-        elif user_time in self.DOWN_TIME and user_ticket.isnumeric():
-            INDEX = self.DOWN_TIME.index(self.ids.time_check.text)
-            print(INDEX)
+    def check_time_and_ticket(self, widget):
+        self.user_time = str(self.ids.time_check.text)
+        self.user_ticket = str(self.ids.ticket_check.text)
+        if self.user_time in self.UP_TIME and self.user_ticket.isnumeric():
+            self.dialog = False
+            INDEX = self.UP_TIME.index(self.user_time)
+            INDEX = INDEX + 1
+            free_ticket = self.free_ticket(int(self.user_ticket))
+            self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX), freetick=int(free_ticket))
+
+        elif self.user_time in self.DOWN_TIME and self.user_ticket.isnumeric():
+            self.dialog = False
+            INDEX = self.DOWN_TIME.index(self.user_time)
+            INDEX = INDEX + 1
+            free_ticket = self.free_ticket(int(self.user_ticket))
+            self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX), freetick=int(free_ticket))
 
         else:
             self.animated_ticket_button(widget)
@@ -135,6 +169,19 @@ class SecondWindow(Screen):
         elif not user_ticket.isdigit():
             animate.start(widget)
 
+    def yes_button_dialog(self, obj):
+        """BRAIN OF THE SCRIPT FUCK DEBUGGING FUCKING FINISH THIS FAST NOW BRO"""
+        user_input_ticket = self.user_ticket
+        user_input_time = self.user_time
+        if user_input_time in self.UP_TIME:
+            INDEX = self.UP_TIME.index(self.user_time)
+            free_ticket = self.free_ticket(int(user_input_ticket))
+            "Do SOMETHING"
+        elif user_input_time in self.DOWN_TIME:
+            INDEX = self.DOWN_TIME.index(self.user_time)
+            free_ticket = self.free_ticket(int(user_input_ticket))
+            "DO SOMETHING"
+
 
 class WindowManager(ScreenManager):
     pass
@@ -158,3 +205,12 @@ class Awesomeapp(MDApp):
 
 if __name__ == "__main__":
     Awesomeapp().run()
+
+"""
+    To Ignore debugging message use os
+    import os
+    os.environ["KIVY_NO_CONSOLELOG"] = "1"
+    
+    This works the best
+    
+"""
