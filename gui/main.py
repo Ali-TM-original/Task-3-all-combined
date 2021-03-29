ba9087b85e0cc5239e741a1ff0522da0ba01b547
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-29 11:05:52 +0500

    Update main.py

--- gui/main.py
+++ gui/main.py
@@ -1,9 +1,6 @@
-"""
-  Still under development not ready for external use
-  Visually: 90% Done
-  Backend: 20%
-"""
+import os
 
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
 
 from kivymd.app import MDApp
 from kivy.lang import Builder
@@ -12,6 +9,9 @@ from kivy.animation import Animation
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
+from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class DialogContent(BoxLayout):
@@ -60,6 +60,7 @@ class FirstWindow(Screen):
 class SecondWindow(Screen):
     dialog = False
     dialog2 = False
+    dialog3 = False
 
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -73,17 +74,19 @@ class SecondWindow(Screen):
         self.TEXT_TO_DISPLAY = ''
         self.user_time = ''
         self.user_ticket = 0
+        # self.add_widget(self.tablec)
 
-    def show_dialog(self, jrny: int, tickets: int, freetick: int):
+    def show_dialog(self, jrny: int, tickets: int, freetick: int, price: int = 0):
 
         """Create group creation popup"""
 
         close = MDFlatButton(text="No", on_release=self.close_dialog)
-        proceed = MDFlatButton(text="Yes", on_release=self.yes_button_dialog)
+        proceed = MDFlatButton(text="Yes", on_press=self.close_dialog, on_release=self.yes_button_dialog)
         if not self.dialog:
             self.dialog = MDDialog(
-                text=f"You are about to buy Tickets for journey# {jrny}",
-                title=f"Are you sure you want to buy {tickets} Tickets? You will recieve {freetick} Free Tickets",
+                text=f"You are about to buy Tickets for journey# {jrny}. You will receive {freetick} Free Tickets."
+                     f"Total Price: {price}",
+                title=f"Are you sure you want to buy {tickets} Tickets?",
                 # type="custom",
                 size_hint=(0.5, 1),
                 auto_dismiss=False,  # needed to allow Buttons to operate
@@ -134,19 +137,30 @@ class SecondWindow(Screen):
         if self.user_time in self.UP_TIME and self.user_ticket.isnumeric():
             self.dialog = False
             INDEX = self.UP_TIME.index(self.user_time)
-            INDEX = INDEX + 1
+            INDEX2 = INDEX + 1
             free_ticket = self.free_ticket(int(self.user_ticket))
-            self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX), freetick=int(free_ticket))
+            total_tickets = free_ticket + int(self.user_ticket)
+            if self.UP_TICKET[INDEX] >= total_tickets:
+                cost = int(self.user_ticket) * 25
+                self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX2), freetick=int(free_ticket), price=cost)
+            elif self.UP_TICKET[INDEX] < total_tickets:
+                # Figure out how to call this here
+                pass
 
         elif self.user_time in self.DOWN_TIME and self.user_ticket.isnumeric():
             self.dialog = False
             INDEX = self.DOWN_TIME.index(self.user_time)
-            INDEX = INDEX + 1
+            INDEX2 = INDEX + 1
             free_ticket = self.free_ticket(int(self.user_ticket))
-            self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX), freetick=int(free_ticket))
-
+            total_tickets = free_ticket + int(self.user_ticket)
+            if self.DOWN_TICKET[INDEX] >= total_tickets:
+                cost = int(self.user_ticket) * 25
+                self.show_dialog(tickets=int(self.user_ticket), jrny=int(INDEX2), freetick=int(free_ticket), price=cost)
+            elif self.DOWN_TICKET[INDEX] < total_tickets:
+                # Figure out how to call this here
+                pass
         else:
-            self.animated_ticket_button(widget)
+            pass
 
     def animated_ticket_button(self, widget):
         user_time = str(self.ids.time_check.text)
@@ -169,18 +183,87 @@ class SecondWindow(Screen):
         elif not user_ticket.isdigit():
             animate.start(widget)
 
-    def yes_button_dialog(self, obj):
-        """BRAIN OF THE SCRIPT FINISH THIS FAST NOW BRO"""
+    def yes_button_dialog(self, xobj):
+        """BRAIN OF THE SCRIPT FUCK DEBUGGING FUCKING FINISH THIS FAST NOW BRO"""
         user_input_ticket = self.user_ticket
         user_input_time = self.user_time
         if user_input_time in self.UP_TIME:
             INDEX = self.UP_TIME.index(self.user_time)
             free_ticket = self.free_ticket(int(user_input_ticket))
-            "Do SOMETHING"
+            total_tickets = free_ticket + int(user_input_ticket)
+            if self.UP_TICKET[INDEX] >= total_tickets:
+                cost = int(user_input_ticket) * 25
+                self.TOTAL_MONEY[INDEX] += int(cost)
+                self.TOTAL_PASSENGERS_[INDEX] += int(user_input_ticket)
+                self.UP_TICKET[INDEX] -= int(user_input_ticket)
+
+            elif self.UP_TICKET[INDEX] < total_tickets:
+                pass
+
         elif user_input_time in self.DOWN_TIME:
             INDEX = self.DOWN_TIME.index(self.user_time)
             free_ticket = self.free_ticket(int(user_input_ticket))
-            "DO SOMETHING"
+            total_tickets = free_ticket + int(user_input_ticket)
+            if self.DOWN_TICKET[INDEX] >= total_tickets:
+                cost = int(user_input_ticket) * 25
+                self.TOTAL_MONEY[INDEX] += int(cost)
+                self.TOTAL_PASSENGERS_[INDEX] += int(user_input_ticket)
+                self.DOWN_TICKET[INDEX] -= int(user_input_ticket)
+
+            elif self.DOWN_TICKET[INDEX] < total_tickets:
+                pass
+
+    def table(self):
+        self.layout_showSummary = MDBoxLayout(size_hint_y=None, height=400)
+        self.tablec = MDDataTable(column_data=[
+            ("UP TIME", dp(30)),
+            ("DOWN TIME", dp(30)),
+            ("UP TICKETS", dp(30)),
+            ("DOWN TICKETS", dp(30)),
+            ("TOTAL PASSANGERS", dp(30)),
+            ("TOTAL MONEY", dp(30))
+        ], row_data=[
+            (self.UP_TIME[0], self.DOWN_TIME[0], self.UP_TICKET[0], self.DOWN_TICKET[0], self.TOTAL_PASSENGERS_[0],
+             self.TOTAL_MONEY[0]), (
+                self.UP_TIME[1], self.DOWN_TIME[1], self.UP_TICKET[1], self.DOWN_TICKET[1], self.TOTAL_PASSENGERS_[1],
+                self.TOTAL_MONEY[1]),
+            (self.UP_TIME[2], self.DOWN_TIME[2], self.UP_TICKET[2], self.DOWN_TICKET[2], self.TOTAL_PASSENGERS_[2],
+             self.TOTAL_MONEY[2]), (
+                self.UP_TIME[3], self.DOWN_TIME[3], self.UP_TICKET[3], self.DOWN_TICKET[3], self.TOTAL_PASSENGERS_[3],
+                self.TOTAL_MONEY[3])
+
+        ])
+
+    def table_screen(self):
+        """Bind this to the top table-show button to scroll to the Table Screen"""
+        self.layout_showSummary = MDBoxLayout(size_hint_y=None, height=400)
+        self.tablec = MDDataTable(column_data=[
+            ("UP TIME", dp(30)),
+            ("DOWN TIME", dp(30)),
+            ("UP TICKETS", dp(30)),
+            ("DOWN TICKETS", dp(30)),
+            ("TOTAL PASSANGERS", dp(30)),
+            ("TOTAL MONEY", dp(30))
+        ], row_data=[
+            (self.UP_TIME[0], self.DOWN_TIME[0], self.UP_TICKET[0], self.DOWN_TICKET[0], self.TOTAL_PASSENGERS_[0],
+             self.TOTAL_MONEY[0]), (
+                self.UP_TIME[1], self.DOWN_TIME[1], self.UP_TICKET[1], self.DOWN_TICKET[1], self.TOTAL_PASSENGERS_[1],
+                self.TOTAL_MONEY[1]),
+            (self.UP_TIME[2], self.DOWN_TIME[2], self.UP_TICKET[2], self.DOWN_TICKET[2], self.TOTAL_PASSENGERS_[2],
+             self.TOTAL_MONEY[2]), (
+                self.UP_TIME[3], self.DOWN_TIME[3], self.UP_TICKET[3], self.DOWN_TICKET[3], self.TOTAL_PASSENGERS_[3],
+                self.TOTAL_MONEY[3])
+
+        ])
+        self.layout_showSummary.add_widget(self.tablec)
+        if not self.dialog3:
+            self.dialog_2 = MDDialog(
+
+                title="Table",
+                type="custom",
+                content_cls=self.layout_showSummary
+            )
+            self.dialog_2.open()
 
 
 class WindowManager(ScreenManager):
@@ -207,8 +290,10 @@ if __name__ == "__main__":
     Awesomeapp().run()
 
 """
+
     To Ignore debugging message use os
     import os
+
     os.environ["KIVY_NO_CONSOLELOG"] = "1"
     
     This works the best
